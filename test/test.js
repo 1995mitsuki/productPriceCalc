ef44f66d52c822f3fbea2747752482e170bcfd0c
柳瀬充玖 <yanasemitsuki@mitsuki.local>
2020-05-20 22:58:09 +0900

    10%の時の税込価格計算のテストを追加

--- test/test.js
+++ test/test.js
@@ -6,16 +6,28 @@ const sampleData1 = {
   price: 100,
   type: "8%",
 };
+// 消費税込みの価格を追加
+const sampleData1_result = sampleData1
+sampleData1_result.priceIntax = 108;
 
-const sampleData1_result = {
+const sampleData2 = {
   price: 100,
-  type: "8%",
-  priceIntax: 108,
+  type: "10%",
 };
 
+const sampleData2_result = sampleData2
+sampleData2_result.priceIntax = 110;
+
+
+
 describe('#productPriceCalc()', () => {
-  test('正確に価格計算ができる', () => {
+  test('消費税が8%のときに正しく税込み価格の計算ができる', () => {
     expect(productCalc.productPriceCalc(sampleData1)).toBe(sampleData1_result);
   });
+
+  test('消費税が10%のときに正しく税込み価格の計算ができる', () => {
+    expect(productCalc.productPriceCalc(sampleData2)).toBe(sampleData2_result);
+  })
+
 });
 
